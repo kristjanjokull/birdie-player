3e7bdfdf007315f8a43614e7ea5485771d01fb5b
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-10-03 13:58:59 +0000

    button code cleanup

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,31 +1,34 @@
 import { FC } from "react";
 
+type ButtonType = "control" | "sidebar" | "big-play";
+
 type ButtonProps = {
-  type: "control" | "sidebar" | "big-play";
+  type: ButtonType;
   onClick: () => void;
   title: string;
   children: React.ReactNode;
 };
 
-export const Button: FC<ButtonProps> = ({ type, onClick, title, children }) => {
-  if (type === "big-play") {
-    return (
-      <button
-        type="button"
-        className="button button__bigPlay"
-        onClick={onClick}
-        title={title}
-      >
-        {children}
-      </button>
-    );
+const getButtonClassName = (type: ButtonType) => {
+  let className = "button ";
+  switch (type) {
+    case "big-play":
+      className += "button__bigPlay";
+      break;
+    case "sidebar":
+      className += "button__sidebar";
+      break;
+    default:
+      className += "button__control";
   }
+  return className;
+};
+
+export const Button: FC<ButtonProps> = ({ type, onClick, title, children }) => {
   return (
     <button
       type="button"
-      className={`button ${
-        type === "sidebar" ? "button__sidebar" : "button__control"
-      }`}
+      className={getButtonClassName(type)}
       onClick={onClick}
       title={title}
     >
