eae414ad78b0d1750d54231d1a5447b861ae0bce
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-10-04 10:37:49 +0000

    index update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
-import { Gaur } from "./gaur";
 import { Video } from "./components/video/video";
+import "./styles/styles.css";
 
-// Main library exports - these are packaged in your distributable
-export const isOdd = (n: number): boolean => {
-  return !!(n & 1);
-};
-
-export { Gaur, Video };
+export { Video };
