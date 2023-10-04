bc7b69ddbaf5960af2241005017a39abe92ed94e
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-10-04 10:24:48 +0000

    sandbox styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import demoswing from "./assets/demoswing2.mp4";
+import "./styles/sandBoxStyles.css";
 
 import { Video } from "./components/video/video";
 
