afb3c55e31cef1f4c79724971bfc6c98c7d9864e
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-10-07 19:48:46 +0000

    update

--- src/components/video/video.stories.tsx
+++ src/components/video/video.stories.tsx
@@ -5,8 +5,29 @@ import { Video } from "@/components/video/video";
 import demoswing from "@/assets/demoswing2.mp4";
 import "@/assets/styles/styles.css";
 
+type ControlProps = {
+  width: string;
+  height: string;
+};
+
 export default {
   title: "Video",
 };
-export const VideoComponent: Story = () => <Video src={demoswing} />;
+export const VideoComponent: Story<ControlProps> = ({ width, height }) => (
+  <Video src={demoswing} width={width} height={height} />
+);
 VideoComponent.storyName = "Video";
+
+VideoComponent.args = {
+  width: "100%",
+  height: "auto",
+};
+
+VideoComponent.argTypes = {
+  width: {
+    control: { type: "text" },
+  },
+  height: {
+    control: { type: "text" },
+  },
+};
--- src/components/videoNative/videoNative.stories.tsx
+++ src/components/videoNative/videoNative.stories.tsx
@@ -5,8 +5,29 @@ import { VideoNative } from "@/components/videoNative/videoNative";
 import demoswing from "@/assets/demoswing2.mp4";
 import "@/assets/styles/styles.css";
 
+type ControlProps = {
+  width: string;
+  height: string;
+};
+
 export default {
   title: "Video Native",
 };
-export const VideoComponent: Story = () => <VideoNative src={demoswing} />;
+export const VideoComponent: Story<ControlProps> = ({ width, height }) => (
+  <VideoNative src={demoswing} width={width} height={height} />
+);
 VideoComponent.storyName = "Video Native";
+
+VideoComponent.args = {
+  width: "100%",
+  height: "auto",
+};
+
+VideoComponent.argTypes = {
+  width: {
+    control: { type: "text" },
+  },
+  height: {
+    control: { type: "text" },
+  },
+};
