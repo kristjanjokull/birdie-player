9589046051611c3cdfeac7cbf2b2393f1eb29763
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-10-07 15:25:46 +0000

    video init

---
+++ src/components/video/video.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Video } from "@/components/video/video";
+import "@/assets/styles/styles.css";
+import demoswing from "@/assets/demoswing2.mp4";
+
+export default {
+  title: "Video",
+};
+export const VideoComponent = () => <Video />;
