99089080c9e37de7d105f36bbcf6fa1aec40ba2b
Kristján Jökull Sigurðsson <kristjanjokull@gmail.com>
2023-10-26 11:37:27 +0000

    bigPlay fix

--- src/components/bigPlay/bigPlay.tsx
+++ src/components/bigPlay/bigPlay.tsx
@@ -1,15 +1,17 @@
 import React from "react";
+import { useVideoStore } from "@/utils/state";
 
 import { Button } from "@/components/button/button";
 import { PlayIcon } from "@/icons/play";
 
 export const BigPlay = () => {
+  const { isPlaying, togglePlay } = useVideoStore((state) => ({
+    isPlaying: state.isPlaying,
+    togglePlay: state.togglePlay,
+  }));
+  if (isPlaying) return null;
   return (
-    <Button
-      type="big-play"
-      title="Play"
-      onClick={() => console.log("hugahuga")}
-    >
+    <Button type="big-play" title="Play" onClick={togglePlay}>
       <PlayIcon />
     </Button>
   );
